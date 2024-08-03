8771af09e5399ab4467fa973aec0eac4f44c9967
EhtishamAhmed11 <ehtashamahmed16133@gmail.com>
2024-08-03 11:52:17 +0500

    final fixes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path'
 // ------------------------
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +11,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app,server } from "./socket/socket.js";
 // --------------------------------
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve()
 // -----------------------------------
 dotenv.config();
 app.use(express.json()) //for parsing the incoming requests with JSON payloads(req.body)
@@ -19,10 +21,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/client/dist"))) //use for servving statics files like html css js
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 // -------------------------------------
-// app.get("/", (req, res) => {
-//   res.send("api running");
-// });
 
 server.listen(PORT, () => {
     connectToMongoDB()
